e6d544d3296e1e68e3ae66548db4a0a23a56f391
davedub <dwitus@gmail.com>
2020-04-24 11:11:18 -0700

    items now returning data from params match id

--- src/components/CatDetails.js
+++ src/components/CatDetails.js
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
 
 function CatDetails({ match }) {
     useEffect((props) => {
-        fetchItem()
-        console.log(match)
+      fetchItem()
+      console.log(match)
     })
-    const [item] = useState({})
+    const [items] = useState({})
 
     const fetchItem = async () => {
-        const fetchItem = await Axios.get(
-            `https://api.thecatapi.com/v1/breeds/search?q=beng`,
+        const fetchItem = await fetch(
+            `https://api.thecatapi.com/v1/breeds/search?q=${match.params.id}`,
             {
-                mode: 'cors',
                 headers: {
-                    'Content-Type': 'application/json, charset=utf-8',
-                    'x-api-key': '54415849-f1f8-41e0-9024-a766ad625326',
+                  mode: 'cors',
+                  'Content-Type': 'application/json, charset=utf-8',
+                  'x-api-key': '54415849-f1f8-41e0-9024-a766ad625326',
                 },
-            }
-        )
-        const item = fetchItem.data
-        console.log(item)
+              },
+              )
+            const items = await fetchItem.json()
+            console.log(items)
     }
     return (
         <div>
